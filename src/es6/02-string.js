1e67cd9dfc1e498e4ebe92725dfabf83dbf88e1d
ALEJANDRO VAN DEN BUSSCHE <rocketmedia.es@gmail.com>
2022-12-06 19:16:07 -0500

    Strings - Template Literals y Multilinea

---
+++ src/es6/02-string.js
@@ -0,0 +1,18 @@
+let hello = 'Hello';
+let world = 'World';
+// let epicPhrase = hello + ' ' + world + '!';
+// console.log(epicPhrase)
+
+//TEMPLATE LITERALS
+let epicPhrase2 = `${hello} ${world}!`;
+console.log(epicPhrase2)
+
+//MULTI-LINE STRINGS
+
+let lorem = 'esto es un string \n' + 'esto es otra linea';
+
+let lorem2 = `esto es una frase epica
+esta es la continuación de esa frase epica`;
+
+console.log(lorem);
+console.log(lorem2);
