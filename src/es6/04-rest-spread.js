388df2a8662be8f8f8532ed3d6f8392eb760c943
ALEJANDRO VAN DEN BUSSCHE <rocketmedia.es@gmail.com>
2022-12-06 21:12:12 -0500

    rest spread

---
+++ src/es6/04-rest-spread.js
@@ -0,0 +1,11 @@
+// arrays destructuring
+
+let fruit = ['Apple', 'Banana'];
+let [a, b] = fruit;
+console.log(a, fruit[1]);
+
+// objet destructurig
+
+let user = {usename: 'Oscar', age:34};
+let {usename, age} = user;
+console.log(usename, user.age);
